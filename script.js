b1c4e7cffbb88edbc10b208ed73aa63d3782e8f8
hojinchang <hojinchang1998@gmail.com>
2023-09-29 13:13:33 -0700

    Add skeleton code for player and game board objects

---
+++ script.js
@@ -0,0 +1,12 @@
+const Player = (name, sign) => {
+    const getName = () => name;
+    const getSign = () => sign;
+
+    return {getName, getSign};
+}
+
+const gameBoard = (function() {
+   const board = ["", "", "", "", "", "", "", "", ""];
+
+   
+})();
