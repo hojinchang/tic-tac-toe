267cb7d2e96a4872c689389a3b4ae80d70b83163
hojinchang <hojinchang1998@gmail.com>
2023-09-30 03:18:20 -0700

    Add winning logic
    The checkWin() method checks whether the current board's state contains a winning combination or not.

--- script.js
+++ script.js
@@ -7,17 +7,27 @@ const Player = (sign) => {
 }
 
 const gameBoard = (function() {
-    const board = ["", "", "", "", "", "", "", "", ""];
+    // const board = ["", "", "", "", "", "", "", "", ""];
+
+    // let board = ["X", "", "", "", "", "", "", "", ""];
+    // let board = ["X", "O", "X", "O", "O", "X", "O", "X", "X"];
+    let board = ["X", "O", "X", "X", "O", "O", "O", "X", "X"];
+    // let board = ["X", "O", "O", "X", "X", "", "X", "", "O"];
+
+
+
+    const getCell = (cellIdx) => board[cellIdx];
 
     const setCell = (cellIdx, sign) => {
         board[cellIdx] = sign;
     }
 
-    return {setCell};
+    return {getCell, setCell};
 })();
 
 const gameController = (function() {
     const players = [Player("X"), Player("O")];
+    let winner = false;
 
     let activePlayer = players[0]
 
@@ -27,10 +37,41 @@ const gameController = (function() {
 
     const getActivePlayer = () => activePlayer;
 
+    const checkWin = () => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        let allEqual = false;
+        for (let condition of winConditions) {
+            let signs = condition.map(cellIdx => gameBoard.getCell(cellIdx));
+            if (!signs.includes("")) allEqual = signs.every(sign => sign === signs[0]);
+            console.log(signs, allEqual)
+
+            if (allEqual) break;
+        }
+        
+        return allEqual;
+    }
+
     const playRound = (cellIdx) => {
         activePlayer = getActivePlayer();
         gameBoard.setCell(cellIdx, activePlayer.getSign());
+        winner = checkWin();
+
+        console.log("winner?", winner)
 
         switchActivePlayer();
     }
+
+    return {playRound};
 })();
+
+gameController.playRound();
