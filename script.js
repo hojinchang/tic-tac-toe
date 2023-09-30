da3c948453867e2511821a23b36c872bb94b2e92
hojinchang <hojinchang1998@gmail.com>
2023-09-29 21:09:57 -0700

    Add player selection game logic

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const Player = (name, sign) => {
-    const getName = () => name;
+"use strict";
+
+const Player = (sign) => {
     const getSign = () => sign;
 
-    return {getName, getSign};
+    return {getSign};
 }
 
 const gameBoard = (function() {
-   const board = ["", "", "", "", "", "", "", "", ""];
+    const board = ["", "", "", "", "", "", "", "", ""];
 
-   
+    const setCell = (cellIdx, sign) => {
+        board[cellIdx] = sign;
+    }
+
+    return {setCell};
+})();
+
+const gameController = (function() {
+    const players = [Player("X"), Player("O")];
+
+    let activePlayer = players[0]
+
+    const switchActivePlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const playRound = (cellIdx) => {
+        activePlayer = getActivePlayer();
+        gameBoard.setCell(cellIdx, activePlayer.getSign());
+
+        switchActivePlayer();
+    }
 })();
